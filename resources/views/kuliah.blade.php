036c3c61381f27e988be3b3612a1de8b806ea841
rgpeople <faizradhitya02@gmail.com>
2023-03-08 19:27:49 +0700

    Praktikum 3 No 2

---
+++ resources/views/kuliah.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('content')
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Pengalaman Kuliah</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Pengalaman Kuliah</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+    
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="small-box bg-light" style="width: 200px auto">
+            <div class="inner">
+                <center>
+                    <h4 style="font-weight: bold">Pengalaman Kuliah</h4><br><br>
+                    <img src="dist/img/faiz.jpg" alt="foto saya" style="width:200px"> <br><br>    
+                    <h4 style="font-weight: bold">Faiz Atha Radhitya | 2141720229</h4>
+                    <p" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde ducimus delectus odit quod itaque! Dolorem blanditiis, 
+                    quia voluptatum consectetur reiciendis magni quod porro nulla deserunt repudiandae reprehenderit dolorum a pariatur ex deleniti fugiat quae nam cum ab quibusdam architecto. 
+                    Quod est ratione enim velit, aperiam atque, quibusdam corporis minus pariatur iusto tenetur explicabo incidunt saepe accusantium sunt libero rerum quia dignissimos cupiditate. 
+                    Expedita porro illum praesentium pariatur eveniet rerum eligendi, a laboriosam ipsam voluptate soluta error odio voluptatem blanditiis dolor. 
+                    Obcaecati repellendus laborum fugit sit ut optio nam iste corrupti mollitia commodi. Esse commodi nulla fuga fugit, qui quam enim, id possimus perferendis vitae, quaerat blanditiis quia maiores quis odio modi assumenda sint. 
+                    Quisquam voluptas maiores tenetur quis quae, molestias, earum totam consequatur, temporibus corrupti ipsam recusandae. Autem, sit aliquam.
+
+                    SANTAI DULU GAK SIHHH.......
+                    </p>
+                </center>
+            </div>
+        </div>
+        <!-- /.content -->
+    </div>
+</section>
+  <!-- /.content-wrapper -->
+@endsection
